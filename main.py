d1d2021799a61644a2d99eca59db71151f4472ea
LaurentBartez <post.thomas@familieknorr.de>
2023-09-15 23:23:23 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import streamlit as st
+from streamlit_chat import message
+import responseGenerator as rg
+import tempfile
+
+def generate_response(prompt, responseGenerator):
+    st.session_state['history'].append({"role": "user", "content": prompt})
+    return responseGenerator.getResponse(prompt)
+
+st.title("Talk to that file")
+uploaded_file = st.sidebar.file_uploader("Upload File", type=["pdf"])
+
+if uploaded_file:
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        tmp_file.write(uploaded_file.getvalue())
+        tmp_file_path = tmp_file.name
+
+    gener = rg.responseGenerator(tmp_file_path)
+    
+    # Initialize chat history
+    if 'history' not in st.session_state:
+        st.session_state['history'] = []
+
+    # Initialize messages
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = ["Hello ! Ask me about " + uploaded_file.name + " 🤗"]
+
+    if 'past' not in st.session_state:
+        st.session_state['past'] = ["Hey ! 👋"]
+
+
+    response_container = st.container()
+    container = st.container()
+
+    with container:
+        with st.form(key='my_form', clear_on_submit=True):
+            user_input = st.text_input("Query:", placeholder="Enter your question...", key='input')
+            submit_button = st.form_submit_button(label='Send')
+
+    if submit_button and user_input:
+        output = generate_response(user_input, gener)
+        st.session_state['past'].append(user_input)
+        st.session_state['generated'].append(output)
+
+
+    if st.session_state['generated']:
+        with response_container:
+            for i in range(len(st.session_state['generated'])):
+                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+                message(st.session_state["generated"][i], key=str(i))
---
+++ responseGenerator.py
@@ -0,0 +1,55 @@
+from langchain.llms import GPT4All
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.chains import ConversationalRetrievalChain
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.embeddings import GPT4AllEmbeddings
+import DocumentLoader
+
+
+class responseGenerator():
+
+    def __init__(self, pathOfDocument):
+        self.documents = self.splitDocuments(pathOfDocument)
+        self.local_path = (
+        "./models/llama-2-7b-chat.ggmlv3.q4_0.bin"  # replace with your desired local file path
+        )
+        self.callbacks = [StreamingStdOutCallbackHandler()]
+        self.chain = self.makeChain()
+        self.chatHistory = []        
+
+    def splitDocuments(self, path):
+        loadedDocuments = DocumentLoader.loadPdf(path)
+ 
+        # we split the data into chunks of 1,000 characters, with an overlap
+        # of 200 characters between the chunks, which helps to give better results
+        # and contain the context of the information between chunks
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        return text_splitter.split_documents(loadedDocuments)
+
+    def makeChain(self):
+        llm = GPT4All(model=self.local_path, callbacks=self.callbacks, verbose=True)
+
+        vectordb = Chroma.from_documents(
+          self.documents,
+          embedding=GPT4AllEmbeddings(),
+          persist_directory='./data'
+        )
+        vectordb.persist()
+
+        retriever = vectordb.as_retriever(search_type ="similarity", search_kwargs={'k': 2})
+
+        chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        chain_type="stuff",
+        retriever=retriever,
+        return_source_documents=True,
+        verbose = True
+        )
+        return chain
+
+    def getResponse(self, prompt):
+        result = self.chain({'question': prompt, 'chat_history': self.chatHistory})
+        self.chatHistory = [prompt, result['answer']]
+        return result['answer']
+    
